b712036f62b0e8ecfffa4561d27edca0786355df
dooyoungk <dackdoo@naver.com>
2020-06-01 23:23:30 +0900

    tutorial part2+part3

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# Use include() to add paths from the catalog application
+from django.urls import path, include
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog /')),
+] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+ 
+
